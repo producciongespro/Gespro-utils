c8480703a196b6a1ab85bb990165fce7d5c30adb
lchaconc <luis.chacon.campos@gmail.com>
2021-02-17 21:53:58 -0600

    Se elimina comentarios

--- src/buscador.js
+++ src/buscador.js
@@ -1,13 +1,12 @@
 
-exports.buscar =  function (palabra, array) {
-    //console.log("array desde buscador", array);
+exports.buscar =  function (palabra, array) {    
     let tmpArray=[];
     palabra= palabra.toLowerCase();
     palabra=eliminarTildes(palabra);
     //console.log("palabra ingresada:", palabra);
     const limite= array.length;
     for (let index = 0; index < limite; index++) {
-      //Si las estiquetas contienen mayúsucla
+      //Si las estiquetas contienen mayúscula:
       //let str = array[index].tags.toLowerCase();
       let str = array[index].tags;
       //str= eliminarTildes(str);
@@ -21,7 +20,7 @@ exports.buscar =  function (palabra, array) {
   }
   
   const  eliminarTildes=(cadena)=> {
-	  console.log("cadena",cadena);
+	  console.log("cadena:",cadena);
     let tmpCadena = cadena.replace(/á/g, "a");
     tmpCadena = tmpCadena.replace(/é/g, "e");    
     tmpCadena = tmpCadena.replace(/í/g, "i");
--- src/test.js
+++ src/test.js
@@ -1,4 +1,15 @@
-const {dispositivo} = require("./dispositivo");
+const { buscar } = require("./buscador");
 
-console.log( dispositivo() );
+let recursos = [
+  {
+    nombre: "Aprendo a leer con la niña Pochita",
+    tags: "lectura, primero, español, básico",
+  },
+  {
+    nombre: "Sumando con los patos saltarines",
+    tags: "sumas, primero, matematica, básico",
+  },
+];
 
+let filtrados = buscar("sumas", recursos);
+console.log("filtrados", filtrados);
