639757b2d2e644e1c6bb3c68ae4689e67ee3fa85
lchaconc <luis.chacon.campos@gmail.com>
2021-03-11 08:25:05 -0600

    Create crud_array.js
    -Se agregar método para insertar objeto en array

---
+++ src/crud_array.js
@@ -0,0 +1,15 @@
+exports.eliminarObjeto = function (id, array) {
+  id = parseInt(id);
+  //console.log("id desde crud", id);
+  //console.log("array desde crud", array);  
+  let eliminado=null;
+
+  for (let index = 0; index < array.length; index++) {
+    if (id === array[index].id ) {
+      //console.log("Posicion", index, "objeto a eliminar", array[index] );      
+    eliminado = array.splice(index, 1 )
+    }    
+  }
+  
+  return eliminado;
+}
