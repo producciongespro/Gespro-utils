7bff4a9547c3370eb4802add216ad67d609f9fd1
lchaconc <luis.chacon.campos@gmail.com>
2021-04-20 21:46:38 -0600

    Update akiri.js
    se agrega método envio de formData

--- src/akiri.js
+++ src/akiri.js
@@ -28,3 +28,20 @@ exports.sendData = async function (url, data) {
   let json= await resp.json();
     return json;
 };
+
+exports.sendFormData = async  (url, items) => {
+    const formData = new FormData();
+  
+    for (let index = 0; index < items.length; index++) {
+      formData.append(items[index].name, items[index].val);
+      console.log("items[index].name", items[index].name);
+      console.log("items[index].val", items[index].val);
+    }
+  
+    const resp = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+    let json = await resp.json();
+    return json;
+  };
